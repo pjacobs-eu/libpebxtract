2d5ca9b7ccf43fc23378f5e409eff493dc1e79f6
Philip Jacobs <contact@pjacobs.eu>
2024-08-23 15:16:26 +0200

    peb extraction lib

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+use memflow::prelude::v1::*;
+use memflow_win32::prelude::v1::*;
+
+#[no_mangle]
+pub extern "C" fn get_cod_peb() -> u64 {
+
+    let inventory = Inventory::scan();
+    let connector = inventory.builder()
+        .connector_chain(
+            ConnectorChain::new(
+                std::iter::once((0, "qemu")),
+                std::iter::empty()).unwrap()
+        )
+        .build()
+        .expect("Could not initialize qemu connector");
+
+    let os = Win32Kernel::builder(connector)
+        .build_default_caches()
+        .build()
+        .expect("Could not initialize win32 kernel");
+
+
+    let process = os
+        .into_process_by_name("cod.exe")
+        .expect("Unable to find process");
+
+    process.proc_info.peb_native.unwrap().to_umem() as u64
+}
